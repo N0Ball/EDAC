d615a5e45a6c6863d5d6c643ffb09eeb86d6b093
N0Ball <cwj0325@g.ncu.edu.tw>
2022-05-20 11:20:00 +0800

    TEST: add test for no noise

---
+++ tests/test_no_noise.py
@@ -0,0 +1,14 @@
+import unittest
+
+from tests.testbase import TestBase
+
+from modules.noise.methods.none import NoNoise
+
+class TestNoNoise(TestBase):
+
+    NO_NOISE = NoNoise(False)
+
+    @TestBase.parameterized
+    def test_default(self, data, expected):
+
+        self.assertEqual(data, expected, self.assert_message)
