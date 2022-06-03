1918744c3e3d69a07bcc194e7c7e020fae220f40
N0Ball <n0ball0325@gmail.com>
2022-06-04 01:55:09 +0800

    FIX: fix problem caused by api change

--- server/web/module/edactest.py
+++ server/web/module/edactest.py
@@ -1,3 +1,4 @@
+from Crypto.Util.number import bytes_to_long, long_to_bytes
 
 from modules.edac.factory import EDACFactory
 from modules.edac.schema import EDACType
@@ -6,7 +7,7 @@ from modules.noise.schema import NoiseMethod
 class EDACTest:
 
     def __init__(self, msg: str, edac_type: EDACType, noise_method: NoiseMethod) -> None:
-        self.MSG = msg.encode('utf8')
+        self.MSG = bytes_to_long(msg.encode('utf8'))
         self.EDAC_TYPE = edac_type
         self.NOISE_METHOD = noise_method
 
@@ -19,6 +20,8 @@ class EDACTest:
         noise_msg = noise_system.add_noise(encode_msg)
 
         pass_check, decode_msg, error_bits = edac_system.decode(noise_msg)
+
+        decode_msg = long_to_bytes(decode_msg)
 
         return {
             'original': str(self.MSG)[2:-1],
